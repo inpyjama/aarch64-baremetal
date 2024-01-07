aa867577053083e28b1dcc6ad8d4ce539c8101cc
Mahmad Bharmal <16340861+Mbharmal@users.noreply.github.com>
2024-01-07 22:10:28 +0530

    Adds support for basic printf
    Signed-off-by: Mahmad Bharmal <16340861+Mbharmal@users.noreply.github.com>

--- Makefile
+++ Makefile
@@ -3,11 +3,14 @@
 # Author: Piyush Itankar <piyush@inpyjama.com>
 #
 
+INCLUDES := -Ilib
+
 img:
 	@echo "Building image for RPi 4+"
-	@aarch64-linux-gnu-gcc -Wall -nostdlib -nostartfiles -ffreestanding -g -c main.c -o main.o
-	@aarch64-linux-gnu-gcc -Wall -nostdlib -nostartfiles -ffreestanding -g -c start.S -o start.o
-	@aarch64-linux-gnu-ld -g -m aarch64elf -nostdlib -T linker.ld main.o start.o -o kernel8.elf
+	@aarch64-linux-gnu-gcc $(INCLUDES) -Wall -nostdlib -nostartfiles -ffreestanding -g -c lib/printf.c -o printf.o
+	@aarch64-linux-gnu-gcc $(INCLUDES) -Wall -nostdlib -nostartfiles -ffreestanding -g -c main.c -o main.o
+	@aarch64-linux-gnu-gcc $(INCLUDES) -Wall -nostdlib -nostartfiles -ffreestanding -g -c start.S -o start.o
+	@aarch64-linux-gnu-ld -g -m aarch64elf -nostdlib -T linker.ld printf.o main.o start.o -o kernel8.elf
 	@aarch64-linux-gnu-objcopy kernel8.elf -O binary kernel8.img
 	@echo "generated: kernel8.img"
 	@echo "Zipping content"
--- main.c
+++ main.c
@@ -3,8 +3,18 @@
  * Author: Mahmad Bharmal <mahmad@inpyjama.com>
  */
 
+#include "printf.h"
+
 #define w32(addr, value) *((volatile unsigned int *) addr) = value
 #define r32(addr) *((volatile unsigned int *) addr)
+
+int (*__read_char__)(void);
+void (*__write_char__)(char c);
+
+void uart_putc (const char c);
+
+void set_read_char(int (*func)(void)) { __read_char__ = func; }
+void set_write_char(void (*func)(char)) { __write_char__ = func; }
 
 void uart_init (void) {
     w32(0xFE215004, 1);
@@ -17,6 +27,10 @@ void uart_init (void) {
     w32(0xFE215068, ((200000000/(115200*8))-1));
     w32(0xFE200004, ((2 << 12) | (2 << 15)));
     w32(0xFE215060, 3);
+
+    // Register UART Tx function
+    set_write_char(uart_putc);
+
 }
 
 void uart_putc (const char c) {
@@ -24,27 +38,17 @@ void uart_putc (const char c) {
     w32(0xFE215040, (unsigned int) c);
 }
 
-void uart_puts(const char* s) {
-    while (*s != '\0') {
-        uart_putc(*s);
-        if (*s == '\n') {
-            uart_putc('\r');
-        }
-        s++;
-    }
-}
-
 void main()
 {
     uart_init();
-    uart_putc('\n');
-    uart_puts("   ______\n");
-    uart_puts("  /  ||  \\\n");
-    uart_puts(" /   /\\   \\\n");
-    uart_puts("/___/  \\___\\\n");
-    uart_putc('\n');
-    uart_puts(" Welcome to inpyjama.com!\n");
-    uart_puts(" YouTube: https://tinyurl.com/inpyjama-aarch64\n");
+    printf("\r\n");
+    printf("   ______\r\n");
+    printf("  /  ||  \\\r\n");
+    printf(" /   /\\   \\\r\n");
+    printf("/___/  \\___\\\r\n");
+    printf("\r\n");
+    printf(" Welcome to inpyjama.com!\r\n");
+    printf(" YouTube: https://tinyurl.com/inpyjama-aarch64\r\n");
 
     while(1);
 }
